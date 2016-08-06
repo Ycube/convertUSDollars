65391d799204ecdc1aac8e734fecd0ff7db186ed
Yale Yuen <yaleyyuen@gmail.com>
2016-08-05 23:13:33 -0700

    [feat] implement disable on  minus buttons

--- src/containers/inputForm.js
+++ src/containers/inputForm.js
@@ -18,10 +18,11 @@ class InputForm extends Component {
   render() {
     let usd = this.props.rates.usd;
 
+    let disabled = usd <= 0 ? 'disabled' : '';
     return (
       <div>
         {/************Dollar Display**************/}
-        <MoneyDispaly money={ usd } />
+        <MoneyDispaly symbol='$' money={ usd } />
         
         <span>Click to add or subtract from Dollars to convert to Euros</span>
         <div className="row">
@@ -29,19 +30,39 @@ class InputForm extends Component {
           <div className="col s6">
             <ul>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" onClick={this.handleClick.bind(this, -1)}><i className="material-icons">-1</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" 
+                   disabled={ disabled } 
+                   onClick={usd > 0 ? this.handleClick.bind(this, -1) : null}>
+                     <i className="material-icons">-1</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" onClick={this.handleClick.bind(this, -5)}><i className="material-icons">-5</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" 
+                   disabled={ disabled } 
+                   onClick={this.handleClick.bind(this, -5)}>
+                  <i className="material-icons">-5</i>
+                </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" onClick={this.handleClick.bind(this, -10)}><i className="material-icons">-10</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" 
+                   disabled={ disabled } 
+                   onClick={this.handleClick.bind(this, -10)}>
+                   <i className="material-icons">-10</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" onClick={this.handleClick.bind(this, -20)}><i className="material-icons">-20</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" 
+                   disabled={ disabled } 
+                   onClick={this.handleClick.bind(this, -20)}>
+                   <i className="material-icons">-20</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" onClick={this.handleClick.bind(this, -100)}><i className="material-icons">-100</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light red lighten-2" 
+                   disabled={ disabled } 
+                   onClick={this.handleClick.bind(this, -100)}>
+                   <i className="material-icons">-100</i>
+                 </a>
               </li>
             </ul>
           </div>
@@ -49,19 +70,34 @@ class InputForm extends Component {
           <div className="col s6">
             <ul>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" onClick={this.handleClick.bind(this, 1)}><i className="material-icons">+1</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" 
+                   onClick={this.handleClick.bind(this, 1)}>
+                   <i className="material-icons">+1</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" onClick={this.handleClick.bind(this, 5)}><i className="material-icons">+5</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" 
+                   onClick={this.handleClick.bind(this, 5)}>
+                   <i className="material-icons">+5</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" onClick={this.handleClick.bind(this, 10)}><i className="material-icons">+10</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" 
+                   onClick={this.handleClick.bind(this, 10)}>
+                   <i className="material-icons">+10</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" onClick={this.handleClick.bind(this, 20)}><i className="material-icons">+20</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" 
+                   onClick={this.handleClick.bind(this, 20)}>
+                   <i className="material-icons">+20</i>
+                 </a>
               </li>
               <li>
-                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" onClick={this.handleClick.bind(this, 100)}><i className="material-icons">+100</i></a>
+                <a className="btn-floating btn-large waves-effect waves-light teal lighten-2" 
+                   onClick={this.handleClick.bind(this, 100)}>
+                   <i className="material-icons">+100</i>
+                 </a>
               </li>
             </ul>
           </div>
