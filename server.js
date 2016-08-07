72eca85ee951c8c22c5f9f246c5016f544dc0970
Yale Yuen <yaleyyuen@gmail.com>
2016-08-07 01:45:28 -0700

    [fix] working on heroku deploy

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080
+
+// using webpack-dev-server and middleware in development environment
+if(process.env.NODE_ENV !== 'production') {
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpack = require('webpack');
+  const config = require('./webpack.config');
+  const compiler = webpack(config);
+  
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + 'index.html')
+});
+
+app.listen(PORT, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  }
+});
